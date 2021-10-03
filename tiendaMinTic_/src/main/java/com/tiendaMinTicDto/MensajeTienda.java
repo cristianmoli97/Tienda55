5bf1dea112210a997154fbbf8fb566aeaffd5a61
rodkman <87214482+rodkman@users.noreply.github.com>
2021-10-03 16:07:22 -0500

    subida metodo envio mensajes a front de carga csv productos

---
+++ tiendaMinTic_/src/main/java/com/tiendaMinTicDto/MensajeTienda.java
@@ -0,0 +1,19 @@
+package com.tiendaMinTicDto;
+
+public class MensajeTienda {
+	private String message;
+
+	  public MensajeTienda(String message) {
+	    this.message = message;
+	  }
+
+	  public String getMessage() {
+	    return message;
+	  }
+
+	  public void setMessage(String message) {
+	    this.message = message;
+	  }
+}
+
+
