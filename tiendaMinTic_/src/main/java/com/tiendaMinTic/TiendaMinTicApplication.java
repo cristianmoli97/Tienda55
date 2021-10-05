9938dedbe59a167b6ff7b3d0f3593d08db870295
rodkman <87214482+rodkman@users.noreply.github.com>
2021-10-04 21:25:29 -0500

    Agregadas librerias para conexio a base datos sin usar aplications.settings

--- tiendaMinTic_/src/main/java/com/tiendaMinTic/TiendaMinTicApplication.java
+++ tiendaMinTic_/src/main/java/com/tiendaMinTic/TiendaMinTicApplication.java
@@ -2,8 +2,13 @@ package com.tiendaMinTic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@ComponentScan({"com.tiendaMinTic","com.tiendaMinTicDao"})
+@EntityScan("com.tiendaMinTicDto")
 public class TiendaMinTicApplication {
 
 	public static void main(String[] args) {
