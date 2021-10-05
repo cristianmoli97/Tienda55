09a4af22d58eaeb4f606a801d14d470a3e6897ca
rodkman <87214482+rodkman@users.noreply.github.com>
2021-10-04 21:31:02 -0500

    agregado sprigbooaplication

--- tiendaMinTic_/src/test/java/tiendaWeb/tiendaMinTic/TiendaMinTicApplicationTests.java
+++ tiendaMinTic_/src/test/java/tiendaWeb/tiendaMinTic/TiendaMinTicApplicationTests.java
@@ -1,9 +1,12 @@
 package tiendaWeb.tiendaMinTic;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 class TiendaMinTicApplicationTests {
 
 	@Test
