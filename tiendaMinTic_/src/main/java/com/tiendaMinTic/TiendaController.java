850e4bfabd7276ecbea7b059620e2c5c1a32e0c2
rodkman <87214482+rodkman@users.noreply.github.com>
2021-10-06 22:05:42 -0500

    merge tienda controller jose angi roman

--- tiendaMinTic_/src/main/java/com/tiendaMinTic/TiendaController.java
+++ tiendaMinTic_/src/main/java/com/tiendaMinTic/TiendaController.java
@@ -14,7 +14,9 @@ import com.tiendaMinTic.CSV.Servicios.ServicioAutenticar;
 import com.tiendaMinTicDao.ProductosDAO;
 import com.tiendaMinTicDao.ProveedorDao;
 import com.tiendaMinTicDto.ProductosVO;
-import com.tiendaMinTicDto.UsuariosVO; 
+import com.tiendaMinTicDto.UsuariosVO;
+import com.tiendaMinTicDao.ProveedoresDAO;
+import com.tiendaMinTicDto.ProveedoresVO;
 
 @Controller
 public class TiendaController {
@@ -161,7 +163,186 @@ public class TiendaController {
 					  
 
 	  }
+	  
+	// CRUD Proveedores
+	  @GetMapping(value = {"/proveedorescrud"})  
+	      public String updateContact2(Model model) {
+	      	ProveedoresVO proveedor = new ProveedoresVO();
+	          model.addAttribute("proveedor",proveedor);
+	          model.addAttribute("popupActive", "hidden");
+	          model.addAttribute("popupMsj", "");
+	          return "proveedoresForm";
+	   }
+	  
+	//Al hacer click sobre alguno de los botones
+	  @PostMapping("/registrarproveedorform")
+	  public String registrarProveedor(@Valid ProveedoresVO proveedor, @RequestParam("evento_boton_crud_proveedor") String botonCrudProveedor , Model model) {
+         
+		  ProveedoresDAO provDao=new ProveedoresDAO();
+		  
+		  String redireccion = "proveedoresForm";
+		  
+		  switch(botonCrudProveedor) {
+			  case "Consultar":
+				  if(provDao.buscarProveedor(proveedor.getNitProveedor()) != null) {
+					  
+					  model.addAttribute("popupMsj", "Proveedor en base de datos");
+					  
+				  }else {
+					  model.addAttribute("popupMsj", "Proveedor inexistente");
+				  }
+				  proveedor.setDefault(); // pone a cero los datos del proveedor
+				  model.addAttribute("proveedor",proveedor);
+			
+				  model.addAttribute("popupActive", "visible");
+	
+				  
+
+				  break;
+			  case "Crear":
+				  if(provDao.registarProveedor(proveedor)) {
+					  
+					  model.addAttribute("popupMsj", "Proveedor guardado enbase de datos");
+					  
+				  }else {
+					  model.addAttribute("popupMsj", "Proveedor ya registrado o no creado intente nuevamente");
+				  }
+				  proveedor.setDefault(); // pone a cero los datos del proveedor
+				  model.addAttribute("proveedor",proveedor);
+			
+				  model.addAttribute("popupActive", "visible");
+				  
+				  
+				  break;
+				  
+			  case "Actualizar":
+				  
+			  	  if(provDao.actualizarProveedor(proveedor)) {
+			  		  model.addAttribute("popupMsj", "Proveedor actualizado en base de datos");
+					  
+				  }else {
+					  model.addAttribute("popupMsj", "Proveedor no actualizado o inexistente intente nuevamente");
+				  }
+				  proveedor.setDefault(); // pone a cero los datos del proveedor
+				  model.addAttribute("proveedor",proveedor);
+			
+				  model.addAttribute("popupActive", "visible");
+				  
+				  break;
+				  
+			  case "Borrar":
+				  
+			  	  if(provDao.eliminarProveedor(proveedor.getNitProveedor())) {
+			  		  model.addAttribute("popupMsj", "Proveedor eliminado de enbase de datos");
+					  
+				  }else {
+					  model.addAttribute("popupMsj", "Proveedor no eliminado o inexistente");
+				  }
+				  proveedor.setDefault(); // pone a cero los datos del proveedor
+				  model.addAttribute("proveedor",proveedor);
+			
+				  model.addAttribute("popupActive", "visible");
+			  				  
+			default:
+				break;
+				
+		  }
+		  
+		  return redireccion;
+					  
+
+	  }	  
+    // CRUD Usuarios
+    @GetMapping(value = {"/usuarioscrud"})  
+    public String updateContact(Model model) {
+    	UsuariosVO usuario = new UsuariosVO();
+        model.addAttribute("usuario",usuario);
+        model.addAttribute("popupActive", "hidden");
+        model.addAttribute("popupMsj", "");
+        return "usuariosForm";
+ }
+    
+      //Al hacer click sobre alguno de los botones
+	  @PostMapping("/registrarusuarioform")
+	  public String registrarusuario( @Valid UsuariosVO usuario, @RequestParam("evento_boton_crud_usuario") String botonCrudUsuario , Model model) {
+         
+		  UsuariosDao userDao=new UsuariosDao();
+		  
+		  String redireccion = "usuariosForm";
+		  
+		  switch(botonCrudUsuario) {
+			  case "Consultar":
+				  if(userDao.consultarUsuario (usuario.getCedula_usuario()) != null) {
+					  
+					  model.addAttribute("popupMsj", "Usuario en base de datos");
+					  
+				  }else {
+					  model.addAttribute("popupMsj", "Usuario inexistente");
+				  }
+				  usuario.setDefault(); // pone a cero los datos del usuario
+				  model.addAttribute("usuario",usuario);
+			
+				  model.addAttribute("popupActive", "visible");
+	
+				  
+
+				  break;
+			  case "Crear":
+				  if(userDao.registrarPersona(usuario)) {
+					  
+					  model.addAttribute("popupMsj", "Usuario guardado en base de datos");
+					  
+				  }else {
+					  model.addAttribute("popupMsj", "Usuario ya registrado o no creado intente nuevamente");
+				  }
+				  usuario.setDefault(); // pone a cero los datos del usuario
+				  model.addAttribute("usuario", usuario);
+			
+				  model.addAttribute("popupActive", "visible");
+				  
+				  
+				  break;
+				  
+			  case "Actualizar":
+				  
+			  	  if(userDao.modificarUsuarios(usuario)) {
+			  		  model.addAttribute("popupMsj", "Usuario actualizado en base de datos");
+					  
+				  }else {
+					  model.addAttribute("popupMsj", "Usuario no actualizado o inexistente intente nuevamente");
+				  }
+				  usuario.setDefault(); // pone a cero los datos del usuario
+				  model.addAttribute("usuario",usuario);
+			
+				  model.addAttribute("popupActive", "visible");
+				  
+				  break;
+				  
+			  case "Borrar":
+				  
+			  	  if(userDao.eliminarUsuario(usuario.getCedula_usuario())) {
+			  		  model.addAttribute("popupMsj", "Usuario eliminado de enbase de datos");
+					  
+				  }else {
+					  model.addAttribute("popupMsj", "Usuario no eliminado o inexistente");
+				  }
+				  usuario.setDefault(); // pone a cero los datos del usuario
+				  model.addAttribute("usuario",usuario);
+			
+				  model.addAttribute("popupActive", "visible");
+			  				  
+			default:
+				break;
+				
+		  }
+		  
+		  return redireccion;
+					  
+
+	  }
     
 	
 
 }
+
+
