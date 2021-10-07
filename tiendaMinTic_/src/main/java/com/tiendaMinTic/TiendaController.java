069871ccf5fed55ea21c9ec462a35c36d9a5cf68
rodkman <87214482+rodkman@users.noreply.github.com>
2021-10-06 21:23:44 -0500

    Modificado parametros metodo post, agregado condicioanal buscarproveedor en metodos actualizar y crear

--- tiendaMinTic_/src/main/java/com/tiendaMinTic/TiendaController.java
+++ tiendaMinTic_/src/main/java/com/tiendaMinTic/TiendaController.java
@@ -5,12 +5,14 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.tiendaMinTic.CSV.Servicios.ServicioAutenticar;
 import com.tiendaMinTicDao.ProductosDAO;
+import com.tiendaMinTicDao.ProveedorDao;
 import com.tiendaMinTicDto.ProductosVO;
 import com.tiendaMinTicDto.UsuariosVO; 
 
@@ -70,15 +72,18 @@ public class TiendaController {
     
       //Al hacer click sobre alguno de los botones
 	  @PostMapping("/registrarproductoform")
-	  public String registrarusuario(@Valid ProductosVO producto, @RequestParam("evento_boton_crud_producto") String botonCrudProducto , Model model) {
-         
+	  public String registrarusuario(@Valid @ModelAttribute("producto") ProductosVO producto,@ModelAttribute("evento_boton_crud_producto") String botonCrudProducto , Model model) {
+ 
 		  ProductosDAO prodDao=new ProductosDAO();
+		  ProveedorDao provDao = new ProveedorDao();
 		  
 		  String redireccion = "productosForm";
-		  
+		
 		  switch(botonCrudProducto) {
 			  case "Consultar":
+				 
 				  if(prodDao.buscarProducto(producto.getCodigoProducto()) != null) {
+					  
 					  
 					  model.addAttribute("popupMsj", "Producto en base de datos");
 					  
@@ -89,17 +94,22 @@ public class TiendaController {
 				  model.addAttribute("producto",producto);
 			
 				  model.addAttribute("popupActive", "visible");
-	
 				  
 
 				  break;
 			  case "Crear":
-				  if(prodDao.registrarProducto(producto)) {
+				 
+				  if(provDao.buscarProveedor(producto.getNitProveedor()).size() > 0){  // si existe el proveedor procede a regitrar
+					  if(prodDao.registrarProducto(producto)) {
 					  
-					  model.addAttribute("popupMsj", "Producto guardado enbase de datos");
+						  model.addAttribute("popupMsj", "Producto guardado enbase de datos");
 					  
+					  }else {
+						  model.addAttribute("popupMsj", "Producto ya registrado, o no creado intente nuevamente");
+					  }
 				  }else {
-					  model.addAttribute("popupMsj", "Producto ya registrado o no creado intente nuevamente");
+					  
+					  model.addAttribute("popupMsj", "Producto no registrado, Nit proveedor incorrecto");
 				  }
 				  producto.setDefault(); // pone a cero los datos del producto
 				  model.addAttribute("producto",producto);
@@ -110,18 +120,23 @@ public class TiendaController {
 				  break;
 				  
 			  case "Actualizar":
-				  
-			  	  if(prodDao.actualizarProducto(producto)) {
-			  		  model.addAttribute("popupMsj", "Producto actualizado en base de datos");
+
+				  if(provDao.buscarProveedor(producto.getNitProveedor()).size() > 0){  // si existe el proveedor procede a actualizar
+					  if(prodDao.actualizarProducto(producto)) {
+						  model.addAttribute("popupMsj", "Producto actualizado en base de datos");
+					  
+					  }else {
+						  model.addAttribute("popupMsj", "Producto no actualizado o inexistente intente nuevamente");
+					  }
 					  
 				  }else {
-					  model.addAttribute("popupMsj", "Producto no actualizado o inexistente intente nuevamente");
+					  model.addAttribute("popupMsj", "Producto no Actualizado, Nit proveedor incorrecto");
 				  }
+				  
 				  producto.setDefault(); // pone a cero los datos del producto
 				  model.addAttribute("producto",producto);
-			
+		
 				  model.addAttribute("popupActive", "visible");
-				  
 				  break;
 				  
 			  case "Borrar":
@@ -141,7 +156,7 @@ public class TiendaController {
 				break;
 				
 		  }
-		  
+
 		  return redireccion;
 					  
 
