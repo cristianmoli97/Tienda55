64e058b6d4028503630e80d7aab8bfc6eb6a0937
rodkman <87214482+rodkman@users.noreply.github.com>
2021-10-08 16:39:14 -0500

    controlador csv producto

--- tiendaMinTic_/src/main/java/com/tiendaMinTic/ProductosCSVControlador.java
+++ tiendaMinTic_/src/main/java/com/tiendaMinTic/ProductosCSVControlador.java
@@ -3,40 +3,60 @@ package com.tiendaMinTic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
+import org.springframework.web.servlet.ModelAndView;
 
 import com.tiendaMinTic.CSV.Servicios.ServicioUploadCSV;
 import com.tiendaMinTicDto.MensajeTienda;
 
+/**
+ * 
+ * @author ledyn roman moreno castellanso
+ *
+ */
 @RestController
 public class ProductosCSVControlador {
 	
+	// Interfaz cargar archivo csv
+			@GetMapping(value = {"/producto"})
+			  public ModelAndView prodcsv(){
+
+				  ModelAndView mav = new ModelAndView("productos");
+			  
+			  return mav; 
+	}
+			
 	@Autowired
 	ServicioUploadCSV uploadFileCsv;
 	
 	@PostMapping("/UploadProductos")
 	  public ResponseEntity<MensajeTienda> uploadFile(@RequestParam("file") MultipartFile file) {
-		  
-	    String message = "";
+
+	    String message = "Error: no se seleccionó archivo para cargar";
+	    
+
 	    String TYPE = "application/vnd.ms-excel";
 
 	    if (TYPE.equals(file.getContentType())) {
-	      try {
-	    	  uploadFileCsv.save(file);
 
-	        message = "Archivo Cargado: " + file.getOriginalFilename();
-	        return ResponseEntity.status(HttpStatus.OK).body(new MensajeTienda(message));
+	      try {
+	    	  if(uploadFileCsv.save(file)) {
+	    		  message = "Archivo Cargado exitosamente: " + file.getOriginalFilename();
+	  	          return ResponseEntity.status(HttpStatus.OK).body(new MensajeTienda(message));
+	    	  }
+	    	  
+	       
 	      } catch (Exception e) {
 	        message = "Archivo no cargado: " + file.getOriginalFilename() + "!";
 	        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new MensajeTienda(message));
 	      }
 	    }
 
-	    message = "Seleccione archivo csv!";
+	    message = "Error: formato de archivo inválido!";
 	    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MensajeTienda(message));
 	  }
 	
