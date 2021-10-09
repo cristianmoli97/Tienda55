de32634a9415ec91dc1b43aadf4b311326e3877c
Alison Moreno Salinas <91579136+alimoreno07@users.noreply.github.com>
2021-10-09 17:44:32 -0500

    Add files via upload

---
+++ tiendaMinTic_/src/main/java/com/tiendaMinTic/UsuariosControlador.java
@@ -0,0 +1,115 @@
+package com.tiendaMinTic;
+
+import java.util.ArrayList;
+
+import javax.validation.Valid;
+
+import com.tiendaMinTicDao.UsuariosDao;
+import com.tiendaMinTicDto.UsuariosVO;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UsuariosControlador {
+	  
+	// CRUD Usuarios
+	
+    @GetMapping(value = {"/usuarioscrud"})  
+    public String updateContact(Model model) {
+    	UsuariosVO usuario = new UsuariosVO();
+        model.addAttribute("usuario",usuario);
+        model.addAttribute("tableActive", "hidden");
+        model.addAttribute("popupActive", "hidden");
+        model.addAttribute("popupMsj", "");
+        return "usuariosForm";
+ }
+    
+      //Al hacer click sobre alguno de los botones
+	  @PostMapping("/registrarusuarioform")
+	  public String registrarusuario(  @ModelAttribute("usuario") UsuariosVO usuario, @ModelAttribute("evento_boton_crud_usuario") String botonCrudUsuario , Model model) {
+         
+		  UsuariosDao userDao=new UsuariosDao();
+		  
+		  String redireccion = "usuariosForm";
+		  
+		  
+		  switch(botonCrudUsuario) {
+			  case "Consultar":
+				  if(userDao.consultarUsuario (usuario.getCedula()) != null) {
+					  
+					  model.addAttribute("popupMsj", "Usuario en base de datos");
+					  
+				  }else {
+					  model.addAttribute("popupMsj", "Usuario inexistente");
+				  }
+				  usuario.setDefault(); // pone a cero los datos del usuario
+				  model.addAttribute("usuario",usuario);
+			
+				  model.addAttribute("popupActive", "visible");
+				  break;
+				  
+			  case "Crear":
+
+				  if(userDao.consultarUsuario(usuario.getCedula()).size()>0){  //si el usuario no existe lo crea
+					if(userDao.registrarPersona(usuario)){
+					  
+						model.addAttribute("popupMsj", "Usuario guardado en base de datos");
+						
+					}else {
+						model.addAttribute("popupMsj", "Usuario ya registrado o no creado intente nuevamente");
+					}
+					model.addAttribute("popupMsj", "Usuario Creado, ya existe");
+				  }
+				  usuario.setDefault(); // pone a cero los datos del usuario
+				  model.addAttribute("usuario", usuario);
+			
+				  model.addAttribute("popupActive", "visible");
+				  
+				  
+				  break;
+				  
+			  case "Actualizar":
+				  
+			    if(userDao.consultarUsuario(usuario.getCedula()).size()>0){
+					  if(userDao.modificarUsuarios(usuario)){
+			  		  	model.addAttribute("popupMsj", "Usuario actualizado en base de datos");
+					  
+				  	  }else {
+					  model.addAttribute("popupMsj", "Usuario no actualizado, el numero de cedula es incorrecto");
+				  	 }
+					   model.addAttribute("popupMsj", "Usuario no actualizado, no existe");
+					}
+				  usuario.setDefault(); // pone a cero los datos del usuario
+				  model.addAttribute("usuario",usuario);
+			
+				  model.addAttribute("popupActive", "visible");
+				  
+				  break;
+				  
+			  case "Borrar":
+				  
+			  	  if(userDao.eliminarUsuario(usuario.getCedula())) {
+			  		  model.addAttribute("popupMsj", "Usuario eliminado de enbase de datos");
+					  
+				  }else {
+					  model.addAttribute("popupMsj", "Usuario no eliminado o inexistente");
+				  }
+				  usuario.setDefault(); // pone a cero los datos del usuario
+				  model.addAttribute("usuario",usuario);
+			
+				  model.addAttribute("popupActive", "visible");
+			  				  
+			default:
+				break;
+				
+		  }
+		  
+		  return redireccion;
+		}
+	
+}
